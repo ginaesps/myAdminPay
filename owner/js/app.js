87e88be154e17ad83cefd6b4aa347c210812bcf3
Regina <respinosa7@ucol.mx>
2021-01-28 17:40:34 -0600

    hide and show updates

--- owner/js/app.js
+++ owner/js/app.js
@@ -1,3 +1,11 @@
+function hide(objectId){
+    document.getElementById(objectId).style.display = 'none';
+}
+
+function show(objectId){
+    document.getElementById(objectId).style.display = 'block'; // que atributo se le debería asignar?
+}
+
 document.getElementById('btnLogin').addEventListener('click', ()=>{
     let login, pwd; //idd, nombred, edadd;
     
@@ -16,9 +24,36 @@ document.getElementById('btnLogin').addEventListener('click', ()=>{
         .catch(error => console.error('Error:', error))
         .then((json) => {
             //console.log(json.rows[0]);
+            var ownerInfo = {
+                first_name: json.rows[0].first_name,
+                paternal_surname: json.rows[0].paternal_surname,
+                maternar_surname: json.rows[0].maternar_surname,
+                phone_number: json.rows[0].phone_number,
+                email: json.rows[0].email
+            }
             document.getElementById('hello').innerHTML = `
-                <h1>Welcome, ${json.rows[0].first_name}</h1>    
+                <h1>Welcome, ${json.rows[0].first_name}</h1>
+                <input type="number" id="owner_id" disabled value='${json.rows[0].id}'</input>     
             `
         }))
         ;
+})
+
+document.getElementById('btnAssociatedDebtors').addEventListener('click', ()=>{
+    //agregar 
+    show('associatedDebtors');
+})
+
+document.getElementById('btnHideDebtors').addEventListener('click', ()=>{
+    hide('associatedDebtors');
+})
+
+//document.getElementById('d_debtor_id', )
+
+document.getElementById('generate__debt').addEventListener('click', ()=>{
+    show('debt__creation');
+})
+
+document.getElementById('btnGenerateDebt').addEventListener('click', ()=>{
+    hide('debt__creation');
 })
